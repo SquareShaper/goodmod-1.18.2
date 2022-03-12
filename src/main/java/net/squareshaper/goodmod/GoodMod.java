51bd4b8279c5fdfc23f0bcc53c41435739db595d
BuildTools <unconfigured@null.spigotmc.org>
2022-03-12 22:42:58 +0100

    added first Item, and made the approriate classes and files

--- src/main/java/net/squareshaper/goodmod/GoodMod.java
+++ src/main/java/net/squareshaper/goodmod/GoodMod.java
@@ -1,21 +1,16 @@
 package net.squareshaper.goodmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.squareshaper.goodmod.item.GoodItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GoodMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("goodmod");
+	public static final String MOD_ID = "goodmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		GoodItems.registerGoodItems();
 	}
 }
--- src/main/java/net/squareshaper/goodmod/item/GoodItems.java
+++ src/main/java/net/squareshaper/goodmod/item/GoodItems.java
@@ -1,5 +1,21 @@
 package net.squareshaper.goodmod.item;
 
-public class GoodItems {
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.squareshaper.goodmod.GoodMod;
+import org.checkerframework.checker.units.qual.A;
 
+public class GoodItems {
+    public static final Item STONE_ROD = registerItem("stone_rod",new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private  static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(GoodMod.MOD_ID, name), item);
+    }
+
+    public static void registerGoodItems() {
+        GoodMod.LOGGER.info("Registering Good Items for " + GoodMod.MOD_ID);
+    }
 }
