776589852be03adf49e20b229d2729c4fb56204f
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 16:53:12 +0100

    added twelve tiers of cobblestone compaction, now mineable

--- src/main/java/net/squareshaper/goodmod/block/GoodBlocks.java
+++ src/main/java/net/squareshaper/goodmod/block/GoodBlocks.java
@@ -14,29 +14,29 @@ import net.squareshaper.goodmod.item.GoodItemGroup;
 
 public class GoodBlocks {
     public static final Block COMPACT_COBBLESTONE = registerBlock("compact_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(3f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(3f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_DOUBLE_COBBLESTONE = registerBlock("compact_double_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_TRIPLE_COBBLESTONE = registerBlock("compact_triple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(5f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_QUADRUPLE_COBBLESTONE = registerBlock("compact_quadruple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(6f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_QUINTUPLE_COBBLESTONE = registerBlock("compact_quintuple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(7f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_SEXTUPLE_COBBLESTONE = registerBlock("compact_sextuple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(8f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_SEPTUPLE_COBBLESTONE = registerBlock("compact_septuple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(9f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_OCTUPLE_COBBLESTONE = registerBlock("compact_octuple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(10f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_NONUPLE_COBBLESTONE = registerBlock("compact_nonuple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(11f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_DECUPLE_COBBLESTONE = registerBlock("compact_decuple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(12f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_UNDECUPLE_COBBLESTONE = registerBlock("compact_undecuple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(13f).requiresTool()), GoodItemGroup.SQUARE);
     public static final Block COMPACT_DODECUPLE_COBBLESTONE = registerBlock("compact_dodecuple_cobblestone",
-            new Block(FabricBlockSettings.of(Material.STONE).hardness(4f).requiresTool()), GoodItemGroup.SQUARE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(14f).requiresTool()), GoodItemGroup.SQUARE);
 
     private static Block registerBlock(String name, Block block, ItemGroup group){
         registerBlockItem(name, block, group);
